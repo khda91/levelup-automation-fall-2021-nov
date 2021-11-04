d84f22b3868c657cef4ab34edfe8752e25ce541c
Dmitry Khodakovsky <khda911@gmail.com>
2021-11-04 20:46:06 +0300

    added divide to zero example

--- src/main/java/ru/levelp/at/lesson02/git/Application.java
+++ src/main/java/ru/levelp/at/lesson02/git/Application.java
@@ -10,5 +10,6 @@ public class Application {
         System.out.println(String.format("2 ^ 10 = %f", calculator.power(2, 10)));
         System.out.println(String.format("6 * 8 = %f", calculator.multiply(6.0, 8.0)));
         System.out.println(String.format("6 / 8 = %f", calculator.divide(6.0, 8.0)));
+        System.out.println(String.format("6 / 0 = %f", calculator.divide(6.0, 0.0)));
     }
 }
